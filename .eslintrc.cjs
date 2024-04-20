c2b2b36f72fe83a35f5d5712bdc73edec940033c
lincore81 <ghspam.99.lbh81@spamgourmet.com>
2024-04-20 21:18:27 +0200

    Added license/readme/dotfiles

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
+    "plugin:react-refresh/recommended",
+    'plugin:react-hooks/recommended',
+    "plugin:react/jsx-runtime",
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
